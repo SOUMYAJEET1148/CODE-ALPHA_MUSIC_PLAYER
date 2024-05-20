8a5b792ad8bda5d47ecec1eaf27311d1e3e449ec
SOUMYAJEET1148 <163433309+SOUMYAJEET1148@users.noreply.github.com>
2024-05-21 00:02:29 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+    {
+      name: "Rangabati Edm Dj",
+      artist: "Dj Armaan",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+        name: "Dhulia Janda Edm Mix",
+        artist: "Dj Babu",
+        img: "music-2",
+        src: "music-2"
+      },
+      {
+        name: "Hey Jaga",
+        artist: "Kuldeep",
+        img: "music-3",
+        src: "music-3"
+      },
+      {
+        name: "Daddy Mummy",
+        artist: "TM Remix",
+        img: "music-4",
+        src: "music-4"
+      }
+    ];
